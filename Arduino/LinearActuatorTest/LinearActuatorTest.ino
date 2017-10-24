c4746b5817ea3d3a3f70d22b7ccc8de3a15314ff
Prava Dhulipalla <prava@students.olin.edu>
2017-10-24 09:56:28 -0400

    added IR sensor control, need to test

--- Arduino/LinearActuatorTest/LinearActuatorTest.ino
+++ Arduino/LinearActuatorTest/LinearActuatorTest.ino
@@ -11,6 +11,9 @@ const byte linactuator2 = 4;
 const byte linactuator3 = 5;
 String result="";
 
+// need to change in calibrate
+int irthreshold = 400;
+
 Servo myservo1;  // create servo object to control a servo
 Servo myservo2;
 Servo myservo3;  // twelve servo objects can be created on most boards
@@ -22,6 +25,9 @@ void setup() {
   myservo1.attach(linactuator1);
   myservo2.attach(linactuator2);
   myservo3.attach(linactuator3);
+
+  pinMode(irsensor, INPUT);
+  
   Serial.begin(9600);
 }
 
@@ -33,14 +39,17 @@ void loop() {
   
   Serial.println(pos);
   delay(10000);
-  
-  for (pos = 1000; pos <= 2000; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo1.writeMicroseconds(pos);              // tell servo to go to position in variable 'pos'
-    myservo2.writeMicroseconds(pos);
-    myservo3.writeMicroseconds(pos);
-    Serial.println(pos);
-    delay(10);                       // waits 15ms for the servo to reach the position
+
+  // when it detects an object, need to calibrate
+  if (analogRead(irsensor) > threshold) {
+    for (pos = 1000; pos <= 2000; pos += 1) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      myservo1.writeMicroseconds(pos);              // tell servo to go to position in variable 'pos'
+      myservo2.writeMicroseconds(pos);
+      myservo3.writeMicroseconds(pos);
+      Serial.println(pos);
+      delay(10);                       // waits 15ms for the servo to reach the position
+    }
   }
   
   //pos = 2000;

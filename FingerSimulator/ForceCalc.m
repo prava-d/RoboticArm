494f08ca46185b10a851220ce7be05abf6627a63
Benjamin Ziemann <bzavmnus1@gmail.com>
2017-10-31 09:30:09 -0400

    Setting up pages

--- FingerSimulator/ForceCalc.m
+++
@@ -1,9 +0,0 @@
-function [ force ] = ForceCalc(holes)
-%FORCECALC Summary of this function goes here
-%   holes is the GLOBAL position of the holes, not local
-    diffs = [holes(:,1) - holes(:,2)  holes(:,3) - holes(:,2)];
-    forces = .5 * [diffs(:, 1)/norm(diffs(:,1)) diffs(:,2)/norm(diffs(:,2))];
-    force = forces(:, 1) + forces(:, 2);
-
-end
-

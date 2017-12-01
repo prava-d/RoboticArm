1a968fc01ba2166db2811bb8f9701f6b6919e472
William Derksen <wderksen@olin.edu>
2017-11-30 21:00:38 -0500

    Pushing old, disfunctional Newtonian Method Simulation of Pulley Finger

--- Pulley Finger Simulator/JointForces.m
+++ Pulley Finger Simulator/JointForces.m
@@ -5,15 +5,15 @@ function [ JointForces ] = JointForces( motorF, strX, strY, N)
     JointForces = zeros(2,N+1);
     for l = 1:size(strX, 1)
         
-        strdiff_last = [0; 0];
+        %strdiff_last = [0; 0];
         strdiff_next = [strX(l, 2) - strX(l, 1); strY(l, 2) - strY(l, 1)];
         
         for i = 1:N
-            strdiff_last = strdiff_next;
+            strdiff_last = [strX(l, 2*i-1) - strX(l, 2*i); strY(l, 2*i-1) - strY(l, 2*i)];
             strdiff_next = [strX(l, 2*i+2) - strX(l, 2*i+1); strY(l, 2*i+2) - strY(l, 2*i+1)];
-            JointForces(:,i) = JointForces(:,i) + (strdiff_next - strdiff_last)*motorF(l)/2;
+            JointForces(:,i) = JointForces(:,i) + (strdiff_next + strdiff_last)*motorF(l)/2;
         end
-        JointForces(:, end) = JointForces(:, end) - strdiff_next*motorF(l);
+        JointForces(:, end) = JointForces(:, end) - strdiff_next*motorF(l)*2;
     end
 end
 
